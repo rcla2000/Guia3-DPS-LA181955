fec7c6dc3d80dac86a6a7de9281549efd7e885ac
Roberto Carlos López Abarca <rcla200031@gmail.com>
2022-09-10 22:09:33 -0600

    Commit inicial

---
+++ Guia3-DPS-LA181955/App.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import {AppRegistry} from 'react-native';
+import {
+  SafeAreaView,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Image
+} from 'react-native';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'Toyota',
+    src: require('./src/imgs/toyota.jpg'),
+  },
+  {
+    id: '2',
+    title: 'Mazda',
+    src: require('./src/imgs/mazda.jpg'),
+  },
+  {
+    id: '3',
+    title: 'Mitsubishi',
+    src: require('./src/imgs/mitsubishi.jpg'),
+  },
+];
+
+const Item = ({title, img}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>    
+    <Image style={styles.img} source={img} />
+  </View> 
+);
+
+const App = () => {
+  const renderItem = ({item}) => (
+    <Item title={item.title} img={item.src} />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+     <FlatList 
+      data = {DATA}
+      renderItem = {renderItem}
+      keyExtractor = {item => item.id}
+     /> 
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 32,
+  },
+  img: {
+    width: 200,
+    height: 125,
+    borderWidth: 2,
+    borderColor: '#d35647',
+    resizeMode: 'contain',
+    margin: 8
+  }
+});
+
+export default App;
